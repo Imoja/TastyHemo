a1ad579084a6ed928b6294d556ecba70f3e93f61
Imoja <44124173+Imoja@users.noreply.github.com>
2022-11-03 11:00:48 +0000

    revert to last working version

--- Source/TastyHemo/Settings.cs
+++ Source/TastyHemo/Settings.cs
@@ -12,12 +12,12 @@ namespace TastyHemo
 	internal class DefaultValues
 	{
 		public const float TastyHemo_NutritionfromBloodFeed = 0.2f;
-		public const float TastyHemo_NutritionfromHemogenPack = 1.0f;
+		//public const float TastyHemo_NutritionfromHemogenPack = 1.0f;
 	}
 	public class IMJ_Settings : ModSettings
 	{
 		public static float TH_NutritionfromBloodFeed = DefaultValues.TastyHemo_NutritionfromBloodFeed;
-		public static float TH_NutritionfromHemogenPack = DefaultValues.TastyHemo_NutritionfromHemogenPack;
+		//public static float TH_NutritionfromHemogenPack = DefaultValues.TastyHemo_NutritionfromHemogenPack;
 
 		public void DoWindowContents(Rect inRect)
 		{
@@ -27,8 +27,8 @@ namespace TastyHemo
 			options.Label("Food the Biter gains from BloodFeeding on Living Pawns: " + TH_NutritionfromBloodFeed.ToString("0.00") + " (1.0 is 100%)");
 			TH_NutritionfromBloodFeed = options.Slider(TH_NutritionfromBloodFeed, 0.00f, 2.00f);
 			options.Gap();
-			options.Label("Food the Drinker gains from Consuming HemogenPacks : " + TH_NutritionfromHemogenPack.ToString("0.00") + " (1.0 is 100%)");
-			TH_NutritionfromHemogenPack = options.Slider(TH_NutritionfromHemogenPack, 0.00f, 2.00f);
+			//options.Label("Food the Drinker gains from Consuming HemogenPacks : " + TH_NutritionfromHemogenPack.ToString("0.00") + " (1.0 is 100%)");
+			//TH_NutritionfromHemogenPack = options.Slider(TH_NutritionfromHemogenPack, 0.00f, 2.00f);
 
 			options.End();
 		}
@@ -36,6 +36,7 @@ namespace TastyHemo
 		public override void ExposeData()
 		{
 			Scribe_Values.Look(ref TH_NutritionfromBloodFeed, "TastyHemo_NutritionfromBloodFeed");
+			//Scribe_Values.Look(ref TH_NutritionfromHemogenPack, "TastyHemo_NutritionfromHemogenPack");
 		}
 	}
 }
